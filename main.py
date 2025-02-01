46c048f40c055cc6be3bb457e50bb31322226277
ender-orwell <enderOrwell@outlook.com>
2025-02-01 22:05:12 +0100

    adding items and users GET

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/") # path, endpoint
 def home_page():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}") # using a path parameter
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
